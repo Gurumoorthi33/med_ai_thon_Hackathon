83ee872de65774058f06f8dfafefd6dd305ef09a
Gurumoorthi <gurumoorthiemp33@gmail.com>
2026-01-02 20:06:36 +0530

    updated the image

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -38,7 +38,7 @@ export function Navigation() {
           className="flex items-center cursor-pointer group"
         >
           <img
-            src="/logo4.png"
+            src="/logo5.png"
             alt="MedAI-Thon Logo"
             className="h-16 w-auto group-hover:scale-110 group-hover:rotate-3 transition-all duration-300 hover:drop-shadow-lg mix-blend-multiply"
           />
